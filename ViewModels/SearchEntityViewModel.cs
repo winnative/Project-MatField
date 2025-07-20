9f01840eecc0ceb5998697d34992774003b90064
Hassan Kazemi <Kazemi.WinStack@outlook.com>
2025-07-20 17:16:11 +0330

    1. Develope search engine

---
+++ ViewModels/SearchEntityViewModel.cs
@@ -0,0 +1,92 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using Project_MatField.Models;
+using Remotion.Linq.Clauses;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project_MatField.ViewModels
+{
+    public class SearchEntityViewModel : ObservableObject
+    {
+        public enum EntityKind
+        {
+            Research,
+            ResearchGroup,
+            Book,
+            BookGroup
+        }
+
+        private string _id = null!;
+        private string _displayContent = null!;
+        private EntityKind _entityKind;
+
+        public SearchEntityViewModel(Book book)
+        {
+            _entityKind = EntityKind.Book;
+            _id = book.Id;
+            _displayContent = book.Name;
+        }
+
+        public SearchEntityViewModel(Research research)
+        {
+            _entityKind= EntityKind.Research;
+            _id = research.Id;
+            _displayContent = research.Subject;
+        }
+
+        public SearchEntityViewModel(BookGroup bookGroup)
+        {
+            _entityKind = EntityKind.BookGroup;
+            _id = bookGroup.Id;
+            _displayContent = bookGroup.Name;
+        }
+
+        public SearchEntityViewModel(ResearchGroup researchGroup)
+        {
+            _entityKind = EntityKind.ResearchGroup;
+            _id = researchGroup.Id;
+            _displayContent = researchGroup.Name;
+        }
+
+        public string Id
+        {
+            get => _id;
+        }
+
+        public string DisplayContent
+        {
+            get => _displayContent;
+        }
+
+        public EntityKind SearchEntityKind
+        {
+            get => _entityKind;
+        }
+
+        public string DisplayKind
+        {
+            get
+            {
+                if (_entityKind is EntityKind.Research)
+                {
+                    return "تحقیق";
+                }
+                else if (_entityKind is EntityKind.ResearchGroup)
+                {
+                    return "پوشه تحقیق";
+                }
+                else if (_entityKind is EntityKind.Book)
+                {
+                    return "کتاب";
+                }
+                else
+                {
+                    return "پوشه کتاب";
+                }
+            }
+        }
+    }
+}
---
+++ ViewModels/SearchServiceViewModel.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.UI.Xaml;
+using Project_MatField.Models;
+using Realms;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project_MatField.ViewModels
+{
+    public class SearchServiceViewModel
+    {
+        private Realm? _dbContext;
+        public SearchServiceViewModel()
+        {
+            InitializeViewModel();
+        }
+
+        private void InitializeViewModel()
+        {
+            InitializeDbContext();
+            GetAllSearchEntities();
+        }
+
+        private void InitializeDbContext() =>
+            _dbContext = (Application.Current as App)?._serviceProvider
+                .GetService<Realm>()!;
+
+        private void GetAllSearchEntities()
+        {
+            var allBooks = _dbContext?.All<Book>().ToList();
+            var allResearches = _dbContext?.All<Research>().ToList();
+            var allResearchGroups = _dbContext?.All<ResearchGroup>().ToList();
+            var allBookGroups = _dbContext?.All<BookGroup>().ToList();
+
+            allBooks?.ForEach(x => { SearchEntities.Add(new(x)); });
+            allBookGroups?.ForEach(x => { SearchEntities.Add(new(x)); });
+            allResearches?.ForEach(x => { SearchEntities.Add(new(x)); });
+            allResearchGroups?.ForEach(x => { SearchEntities.Add(new(x)); });   
+        }
+
+        public ObservableCollection<SearchEntityViewModel> SearchEntities { get; set; } = null!;
+    }
+}
